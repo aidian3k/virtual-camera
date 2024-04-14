b58c80e11bf261b3a2a01ad1c0254a15df6ca407
Adrian Nowosielski <adrian.nowosielski1@gmail.com>
2024-04-14 18:43:30 +0200

    ending of bsp implementation

--- src/virtual-camera/bsp.py
+++ src/virtual-camera/bsp.py
@@ -33,6 +33,19 @@ def draw_polygon(screen, camera, projection, polygon):
         pygame.draw.polygon(screen, polygon.color, projected_polygon_points)
 
 
+def calculate_intersection(point_a, point_b, polygon):
+    f_a = implicit_plane_function(point_a, polygon)
+    f_b = implicit_plane_function(point_b, polygon)
+
+    if f_a * f_b > 0:
+        return None
+
+    t = abs(f_a) / (abs(f_a) + abs(f_b))
+    intersection_point = point_a + t * (point_b - point_a)
+
+    return intersection_point
+
+
 class BspTree:
     def __init__(self, starting_polygon: Polygon):
         self.polygon = starting_polygon
@@ -76,6 +89,7 @@ class BspTree:
             f_b = 0
         if abs(f_c) < np.finfo(float).eps:
             f_c = 0
+
         if f_a <= 0 and f_b <= 0 and f_c <= 0:
             if self.right is None:
                 self.right = BspTree(polygon)
@@ -86,3 +100,29 @@ class BspTree:
                 self.left = BspTree(polygon)
             else:
                 self.left.add(polygon)
+        else:
+            intersection_ab = calculate_intersection(first_node[:3], second_node[:3], self.polygon)
+            intersection_bc = calculate_intersection(second_node[:3], third_node[:3], self.polygon)
+            intersection_ca = calculate_intersection(third_node[:3], first_node[:3], self.polygon)
+
+            if f_a * f_b < 0:
+                if f_a < 0:
+                    self.left = BspTree(Polygon([first_node, intersection_ab, intersection_ca], polygon.color))
+                    self.right = BspTree(Polygon([second_node, intersection_bc, intersection_ab], polygon.color))
+                else:
+                    self.left = BspTree(Polygon([second_node, intersection_ab, intersection_bc], polygon.color))
+                    self.right = BspTree(Polygon([third_node, intersection_ca, intersection_bc], polygon.color))
+            elif f_a * f_c < 0:
+                if f_a < 0:
+                    self.left = BspTree(Polygon([first_node, intersection_ab, intersection_ca], polygon.color))
+                    self.right = BspTree(Polygon([third_node, intersection_ca, intersection_bc], polygon.color))
+                else:
+                    self.left = BspTree(Polygon([third_node, intersection_ca, intersection_bc], polygon.color))
+                    self.right = BspTree(Polygon([second_node, intersection_bc, intersection_ab], polygon.color))
+            elif f_b * f_c < 0:
+                if f_b < 0:
+                    self.left = BspTree(Polygon([second_node, intersection_bc, intersection_ab], polygon.color))
+                    self.right = BspTree(Polygon([third_node, intersection_ca, intersection_bc], polygon.color))
+                else:
+                    self.left = BspTree(Polygon([third_node, intersection_ca, intersection_bc], polygon.color))
+                    self.right = BspTree(Polygon([first_node, intersection_ab, intersection_ca], polygon.color))
